60816b96e40c7365dd002497af89804ca24920ac
Ford Henley <fordp86@gmail.com>
2022-08-03 15:24:15 -0500

    Final Commit
    Completed the project to reverse a string in JS

--- js/site.js
+++ js/site.js
@@ -1,4 +1,25 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+// Get String Value From The Page
+function getStringValue() {
+  document.getElementById("alert").classList.add("invisible");
+  let userString = document.getElementById("userString").value;
+  let revString = reverseString(userString);
+  displayString(revString);
+}
+
+// Reverse The String
+function reverseString(userString) {
+  let revString = [];
+  // Reverse A String Using A For Loop
+  for (let i = userString.length - 1; i >= 0; i--) {
+    revString += userString[i];
+  }
+  return revString;
+}
+
+// Display Message With Reverse String To The User
+function displayString(revString) {
+    // write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    // show alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
